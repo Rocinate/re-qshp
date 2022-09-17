fb69c22d943644d961db1ecf817a0f8fb8b5206c
Rocinate <youjunjie123456@qq.com>
2022-09-17 23:53:33 +0800

    add login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, LinearProgress } from "@mui/material";
 import { useState } from "react";
 import { useQuery } from "react-query";
 import bgImg from "@/assets/login-bg1.jpg";
@@ -9,7 +9,7 @@ import SwipeableViews from "react-swipeable-views";
 const Clause = () => {
   return (
     <div
-      className="absolute bottom-0 right-0 flex text-white"
+      className="absolute bottom-1 right-1 flex text-white"
       style={{ fontSize: "14px" }}
     >
       <p className="mx-2">使用条款</p>
@@ -19,16 +19,16 @@ const Clause = () => {
 };
 
 const Login = () => {
-  const [progress, setProgress] = useState(1);
-  const [userList, setUserList] = useState([1,2,3]);
+  const [progress, setProgress] = useState(0);
+  const [userList, setUserList] = useState([1, 2, 3]);
 
   const login = (data) => {
     console.log(data);
   };
 
   const chooseUser = (data) => {
-    console.log(data)
-  }
+    console.log(data);
+  };
 
   return (
     <div className="flex h-full">
@@ -43,11 +43,14 @@ const Login = () => {
           <div className="w-full h-full blur-sm bg-opacity-10 bg-black"></div>
         </div>
 
-        <div className="relative z-10 bg-white p-11 box-border" style={{borderRadius: '4px'}}>
-          <SwipeableViews index={progress} style={{ width: "400px" }}>
-            <Form login={login} />
-            <UserList data={userList} choose={chooseUser}/>
-          </SwipeableViews>
+        <div style={{ borderRadius: "4px" }} className='overflow-hidden'>
+          <LinearProgress />
+          <div className="relative z-10 bg-white p-11 box-border">
+            <SwipeableViews index={progress} style={{ width: "400px" }}>
+              <Form login={login} />
+              <UserList data={userList} choose={chooseUser} />
+            </SwipeableViews>
+          </div>
         </div>
 
         <Clause />
