3c6e89e8ae301716d202344d0f554915da2a1172
Rocinate <rocinate1999@gmail.com>
2022-11-16 22:49:31 +0800

    add usercard popover

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,53 @@
-import { Typography } from "@mui/material";
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  CardHeader,
+  Button,
+  Grid,
+  Menu
+} from "@mui/material";
+import PopupState, { bindHover, bindMenu } from "material-ui-popup-state";
+import avatarBg from "@/assets/avatar-bg.jpg";
+import HoverMenu from 'material-ui-popup-state/HoverMenu'
+import { Box } from "@mui/system";
 
 const UserCard = ({ data }) => {
   return (
     <>
-      <Typography variant="subtitle2" className="underline underline-offset-2">
-        {data.author}
-      </Typography>
+      <PopupState variant="popover">
+        {(popupState) => (
+          <Box {...bindHover(popupState)}>
+            <Typography
+              variant="subtitle2"
+              className="underline underline-offset-2"
+            >
+              {data.author}
+            </Typography>
+            <HoverMenu {...bindMenu(popupState)}>
+              <Card>
+                <CardHeader>
+                  {/* <img src={avatarBg} alt="avatar" height={200}/> */}
+                  <Typography>123421</Typography>
+                </CardHeader>
+                <CardContent>
+                  <Grid container spacing={0}>
+                    <Grid item md={4}></Grid>
+                    <Grid item md={4}></Grid>
+                    <Grid item md={4}></Grid>
+                    <Grid item md={4}></Grid>
+                    <Grid item md={4}></Grid>
+                  </Grid>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Zone</Button>
+                </CardActions>
+              </Card>
+            </HoverMenu>
+          </Box>
+        )}
+      </PopupState>
     </>
   );
 };
